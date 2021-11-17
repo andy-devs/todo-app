53c88088650b08f04ee0ae71d853f0b497708e14
andy-devs <markushin.andrew@gmail.com>
2021-11-17 14:22:37 +0600

    dragula replace

--- app.js
+++ app.js
@@ -34,10 +34,6 @@ function bordersUpdate() {
 bordersUpdate();
 
 var todoList = document.querySelectorAll('.todo__block-item');
-// for (let todo of todoList) {
-// 	todo.draggable = true;
-// 	todo.classList.add('draggable');
-// }
 
 const todoCount = document.querySelector('.todo__info-left__count');
 
@@ -270,50 +266,6 @@ todoForm.addEventListener('submit', (e) => {
 // Drag and Drop
 
 dragula([document.querySelector('.container')]);
-
-// var draggables = document.querySelectorAll('.draggable');
-// var container = document.querySelector('.container');
-
-// draggables.forEach((draggable) => {
-// 	draggable.addEventListener('dragstart', (e) => {
-// 		draggable.classList.add('dragging');
-// 	});
-// 	draggable.addEventListener('dragend', (e) => {
-// 		draggable.classList.remove('dragging');
-// 	});
-// });
-
-// container.addEventListener('dragover', (e) => {
-// 	e.preventDefault();
-// 	const afterElement = getDragAfterElement(container, e.clientY);
-// 	const draggable = document.querySelector('.dragging');
-// 	if (afterElement == null) {
-// 		container.appendChild(draggable);
-// 	} else {
-// 		container.insertBefore(draggable, afterElement);
-// 	}
-// 	bordersUpdate();
-// });
-
-// function getDragAfterElement(container, y) {
-// 	const draggableElements = [
-// 		...container.querySelectorAll('.draggable:not(.dragging)'),
-// 	];
-// 	return draggableElements.reduce(
-// 		(closest, child) => {
-// 			const box = child.getBoundingClientRect();
-// 			const offset = y - box.top - box.height / 2;
-// 			if (offset < 0 && offset > closest.offset) {
-// 				return { offset: offset, element: child };
-// 			} else {
-// 				return closest;
-// 			}
-// 		},
-// 		{
-// 			offset: Number.NEGATIVE_INFINITY,
-// 		}
-// 	).element;
-// }
 
 const colorButton = document.querySelector('.todo__head-icon');
 const themeIcon = document.querySelector('.todo__head-icon__item');
