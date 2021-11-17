6276fd5dcb9f4705ba3b16decc8ccd8406d88d9c
andy-devs <markushin.andrew@gmail.com>
2021-11-17 14:21:52 +0600

    dragula add

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ function bordersUpdate() {
 bordersUpdate();
 
 var todoList = document.querySelectorAll('.todo__block-item');
-for (let todo of todoList) {
-	todo.draggable = true;
-	todo.classList.add('draggable');
-}
+// for (let todo of todoList) {
+// 	todo.draggable = true;
+// 	todo.classList.add('draggable');
+// }
 
 const todoCount = document.querySelector('.todo__info-left__count');
 
@@ -269,49 +269,51 @@ todoForm.addEventListener('submit', (e) => {
 
 // Drag and Drop
 
-var draggables = document.querySelectorAll('.draggable');
-var container = document.querySelector('.container');
+dragula([document.querySelector('.container')]);
 
-draggables.forEach((draggable) => {
-	draggable.addEventListener('dragstart', (e) => {
-		draggable.classList.add('dragging');
-	});
-	draggable.addEventListener('dragend', (e) => {
-		draggable.classList.remove('dragging');
-	});
-});
+// var draggables = document.querySelectorAll('.draggable');
+// var container = document.querySelector('.container');
 
-container.addEventListener('dragover', (e) => {
-	e.preventDefault();
-	const afterElement = getDragAfterElement(container, e.clientY);
-	const draggable = document.querySelector('.dragging');
-	if (afterElement == null) {
-		container.appendChild(draggable);
-	} else {
-		container.insertBefore(draggable, afterElement);
-	}
-	bordersUpdate();
-});
+// draggables.forEach((draggable) => {
+// 	draggable.addEventListener('dragstart', (e) => {
+// 		draggable.classList.add('dragging');
+// 	});
+// 	draggable.addEventListener('dragend', (e) => {
+// 		draggable.classList.remove('dragging');
+// 	});
+// });
 
-function getDragAfterElement(container, y) {
-	const draggableElements = [
-		...container.querySelectorAll('.draggable:not(.dragging)'),
-	];
-	return draggableElements.reduce(
-		(closest, child) => {
-			const box = child.getBoundingClientRect();
-			const offset = y - box.top - box.height / 2;
-			if (offset < 0 && offset > closest.offset) {
-				return { offset: offset, element: child };
-			} else {
-				return closest;
-			}
-		},
-		{
-			offset: Number.NEGATIVE_INFINITY,
-		}
-	).element;
-}
+// container.addEventListener('dragover', (e) => {
+// 	e.preventDefault();
+// 	const afterElement = getDragAfterElement(container, e.clientY);
+// 	const draggable = document.querySelector('.dragging');
+// 	if (afterElement == null) {
+// 		container.appendChild(draggable);
+// 	} else {
+// 		container.insertBefore(draggable, afterElement);
+// 	}
+// 	bordersUpdate();
+// });
+
+// function getDragAfterElement(container, y) {
+// 	const draggableElements = [
+// 		...container.querySelectorAll('.draggable:not(.dragging)'),
+// 	];
+// 	return draggableElements.reduce(
+// 		(closest, child) => {
+// 			const box = child.getBoundingClientRect();
+// 			const offset = y - box.top - box.height / 2;
+// 			if (offset < 0 && offset > closest.offset) {
+// 				return { offset: offset, element: child };
+// 			} else {
+// 				return closest;
+// 			}
+// 		},
+// 		{
+// 			offset: Number.NEGATIVE_INFINITY,
+// 		}
+// 	).element;
+// }
 
 const colorButton = document.querySelector('.todo__head-icon');
 const themeIcon = document.querySelector('.todo__head-icon__item');
